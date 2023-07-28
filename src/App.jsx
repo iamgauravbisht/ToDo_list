69211f87427d6c9c43da42efac81819a9e700920
Gaurav <gauravbisht154@gmail.com>
2023-07-29 02:15:14 +0530

    the basic structure of app and functionality is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
 import "./App.css";
+import { useState } from "react";
+import plus from "./assets/Plus.svg";
+import del from "./assets/delete.svg";
 
 function App() {
-  return <div>App</div>;
+  const [textValue, setTextValue] = useState("");
+  const [list, setList] = useState([{ text: "gaurav", id: 1 }]);
+  // const List = [{ text: "Hello", id: 0 }];
+
+  const createListHandler = (e) => {
+    const idValue = new Date();
+    if (textValue == "") return;
+    if (e) {
+      e.preventDefault();
+    }
+    setList((oldList) => {
+      return [...oldList, { text: textValue, id: idValue }];
+    });
+    setTextValue("");
+  };
+
+  const deleteListHandler = (id) => {
+    setList((oldList) => {
+      return [...oldList.filter((el) => el.id !== id)];
+    });
+  };
+  return (
+    <div className="app">
+      <h1>To Do List</h1>
+      <form onSubmit={createListHandler}>
+        <input
+          type="text"
+          value={textValue}
+          onChange={(e) => {
+            setTextValue(e.target.value);
+          }}
+        />
+        <img src={plus} alt="Plus" onClick={createListHandler} />
+      </form>
+      {list.map((item, i) => {
+        return (
+          <div key={i} className="list">
+            <p>{item.text}</p>
+            <div>
+              <button>edit</button>
+              <img
+                src={del}
+                alt="del"
+                onClick={() => deleteListHandler(item.id)}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
